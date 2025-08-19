ed85a1335bbc1114b72adbb30376469e2d03a748
Laura Rost <git@laurarost.de>
2025-08-19 12:10:31 +0200

    compare all line pairs for similar line removal

--- app/src/app/hough_lines.py
+++ app/src/app/hough_lines.py
@@ -1,14 +1,40 @@
 import math
 import cv2 as cv
 import numpy as np
-
-from itertools import groupby
 from zipfile import ZipFile
 
 from util import pillow_image_to_bytes
 from tables.table_crop import pdf_convert_to_images
 
-SIMILAR_LINE_DISTANCE = 200
+SIMILAR_LINE_DISTANCE = 125
+SHORT_LINE_LENGTH = 750
+
+# try removing signal symbol or text lines and similar
+def is_line_similar(line, line_comp):
+    # we want to exclude long lines, they're probably actual edges
+    line_length = math.dist((line[0], line[1]), (line[2], line[3]))
+    if line_length >= SHORT_LINE_LENGTH:
+        return False
+    
+    # check if slopes are similar, else the lines are also not similar
+    delta_y1 = abs(line[3] - line[1])
+    delta_x1 = abs(line[2] - line[0])
+    delta_y2 = abs(line_comp[3] - line_comp[1])
+    delta_x2 = abs(line_comp[2] - line_comp[0])
+    if delta_x1 != 0 and delta_x2 != 0 and abs(delta_y1 / delta_x1 - delta_y2 / delta_x2) > 2:
+        return False
+    
+    # there are usually lots of lines near each other at these symbols
+    startdist = math.dist((line[0], line[1]), (line_comp[0], line_comp[1]))
+    enddist = math.dist((line[2], line[3]), (line_comp[2], line_comp[3]))
+    startdist_switched = math.dist((line[0], line[1]), (line_comp[2], line_comp[3]))
+    enddist_switched = math.dist((line[2], line[3]), (line_comp[0], line_comp[1]))
+    if startdist <= SIMILAR_LINE_DISTANCE and enddist <= SIMILAR_LINE_DISTANCE:
+        return True
+    elif startdist_switched <= SIMILAR_LINE_DISTANCE and enddist_switched <= SIMILAR_LINE_DISTANCE:
+        return True
+    else:
+        return False
 
 def main():
     # load ZIP
@@ -30,34 +56,24 @@ def main():
     cdstP = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)
     
     linesP = cv.HoughLinesP(dst, 1, np.pi / 180, 50, None, 125, 45)
-
-    # first sort by x coordinates, then by y coordinates
-    linesP = sorted(linesP, key = lambda l: (l[0][0], l[0][2], l[0][1], l[0][3]))
     
     if linesP is not None:
         for i in range(0, len(linesP)):
             l = linesP[i][0]
-            lprev = linesP[i-1][0] if i > 0 else None
-            # first sketchy try at removing signal symbol lines and similar
-            # there are usually lots of lines near each other at these symbols
-            # should also check if line lengths are small
-            # should also do the same check with lnext
-            if lprev is not None:
-                startdist = math.dist((l[0], l[1]), (lprev[0], lprev[1]))
-                enddist = math.dist((l[2], l[3]), (lprev[2], lprev[3]))
-                startdist_switched = math.dist((l[0], l[1]), (lprev[2], lprev[3]))
-                enddist_switched = math.dist((l[2], l[3]), (lprev[0], lprev[1]))
-                if (startdist <= SIMILAR_LINE_DISTANCE and enddist <= SIMILAR_LINE_DISTANCE) or (startdist_switched <= SIMILAR_LINE_DISTANCE and enddist_switched <= SIMILAR_LINE_DISTANCE):
+
+            found_flag = False
+            for j in range(0, len(linesP)):
+                if i == j:
                     continue
+                l_comp = linesP[j][0]
+                if is_line_similar(l, l_comp):
+                    found_flag = True
+                    break
+            if found_flag:
+                continue
+
             print(f"({l[0]}, {l[1]}) -- ({l[2]}, {l[3]})")
             cv.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv.LINE_AA)
-
-    # linesPGrouped = groupby(linesP, lambda l: round(l[0][1], -1))
-
-    # groups = [{'type':k, 'items':[x[0] for x in v]} for k, v in linesPGrouped]
-
-    #for group in groups:
-    #    print(f"{group['type']} -> {group['items']}")
 
     cv.imwrite("./detected_probabilistic.jpg", cdstP)
 
